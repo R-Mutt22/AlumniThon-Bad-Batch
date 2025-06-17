82ac83171f6e1cc26798bbd3f9e6e5e9f4a8caed
EV3THlm <thefret7863@gmail.com>
2025-06-16 19:29:31 -0600

    Initial structure

---
+++ batch/src/main/java/com/bad/batch/SkillLinkApplication.java
@@ -0,0 +1,13 @@
+package com.bad.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillLinkApplication.class, args);
+	}
+
+}
